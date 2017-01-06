ffdf9324d9b1e5ab78ab7b5ab2a3af0f3fcc97c4
Thomas Marek <ttmarek@outlook.com>
2017-01-06 15:48:01 -0500

    Update React Native GTM target (#15)

--- src/targets/react-native/google-tag-manager.js
+++ src/targets/react-native/google-tag-manager.js
@@ -1,13 +1,16 @@
-function ReactNativeGoogleTagManager(events, googleTagManager) {
-  events.forEach((event) => {
-    const eventToPush = (() => {
-      if (event.event === undefined && event.hitType !== undefined) {
-        return Object.assign({}, event, { event: event.hitType });
-      }
-      return event;
-    })();
-    googleTagManager.pushDataLayerEvent(eventToPush);
-  });
+function GoogleTagManager(trackingId, GTMBridge) {
+  GTMBridge.openContainerWithId(trackingId);
+  return function GoogleTagManagerTarget(events) {
+    events.forEach((event) => {
+      const eventToPush = (() => {
+        if (event.event === undefined && event.hitType !== undefined) {
+          return Object.assign({}, event, { event: event.hitType });
+        }
+        return event;
+      })();
+      GTMBridge.pushDataLayerEvent(eventToPush);
+    });
+  };
 }
 
-module.exports = { ReactNativeGoogleTagManager };
+module.exports = { GoogleTagManager };
