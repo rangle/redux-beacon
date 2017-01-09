febc795bffa219c3de03d3c1b78303e221d0fea3
Thomas Marek <ttmarek@outlook.com>
2017-01-09 15:03:48 -0500

    Remove unnecessary linter warnings (#20)

--- src/main/create-meta-reducer.js
+++ src/main/create-meta-reducer.js
@@ -2,6 +2,7 @@ const createEvents = require('./create-events');
 const registerEvents = require('./register-events');
 
 function createMetaReducer(eventDefinitionsMap, target, extensions = {}) {
+  /* eslint-disable func-names */
   return function (reducer) {
     return function (prevState, action) {
       if (!eventDefinitionsMap[action.type]) {
