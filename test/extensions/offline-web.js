3a93366856513cc7d967d01d7ee283898e716e57
ttmarek <ttmarek@outlook.com>
2017-01-18 11:50:46 -0500

    More code I forgot to stage

---
+++ test/extensions/offline-web.js
@@ -0,0 +1,25 @@
+const moment = require('moment');
+const { addTimestamp } = require('../../src/extensions/offline-web');
+
+describe('addTimestamp', () => {
+  describe('When given an array of event objects', () => {
+    it('adds a timestamp to each event object', () => {
+      const events = [{ hitType: 'pageview', page: '/home' }];
+
+      const result = addTimestamp(events);
+
+      const timestamp = result[0].timeSaved;
+      expect(moment.isDate(timestamp)).toBe(true);
+      expect(moment(timestamp).fromNow()).toEqual('a few seconds ago');
+    });
+
+    it('leaves the rest of the event intact', () => {
+      const events = [{ hitType: 'pageview', page: '/home' }];
+
+      const resultingEvent = addTimestamp(events)[0];
+
+      expect(resultingEvent.hitType).toBe('pageview');
+      expect(resultingEvent.page).toBe('/home');
+    });
+  });
+});
