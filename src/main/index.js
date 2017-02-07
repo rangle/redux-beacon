329e2de8c429975f3cab22415d52d0520a8dea1f
Thomas Marek <ttmarek@outlook.com>
2017-02-07 14:43:46 -0500

    Expose createEvents for testing event definitions (#47)

--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,9 @@
 const createMiddleware = require('./create-middleware');
 const createMetaReducer = require('./create-meta-reducer');
+const createEvents = require('./create-events');
 
 module.exports = {
   createMiddleware,
   createMetaReducer,
+  createEvents,
 };
