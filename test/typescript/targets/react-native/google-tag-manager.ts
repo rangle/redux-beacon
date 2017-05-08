cbb31c52e3195bf913c24a0e41a2dda31bc8f9a1
Thomas Marek <ttmarek@outlook.com>
2017-05-07 21:14:02 -0400

    Remove test directory

--- test/typescript/targets/react-native/google-tag-manager.ts
+++
@@ -1,8 +0,0 @@
-import {
-  GoogleTagManager,
-} from '../../../../src/targets/react-native/google-tag-manager';
-
-const trackingId = 'UX-XXXXXX';
-const GTMBridge = {};
-
-GoogleTagManager(trackingId, GTMBridge);
