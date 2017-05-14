60b24b99476b40b43516193cafab959aad95db4b
Thomas Marek <ttmarek@outlook.com>
2017-05-13 23:27:23 -0400

    Add tests for build and make fixes

---
+++ src/targets/react-native/index.js
@@ -0,0 +1,7 @@
+const { GoogleAnalytics } = require('./google-analytics');
+const { GoogleTagManager } = require('./google-tag-manager');
+
+module.exports = {
+  GoogleAnalytics,
+  GoogleTagManager,
+};
