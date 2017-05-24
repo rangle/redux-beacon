75043e8a775d9e19c2fca2b11e3defb4b2832259
mfornes <matias.fornes@smava.de>
2017-05-24 11:57:34 +0200

    Fixed: Use new signature on google-tag-manager-node-env.test.js

--- src/targets/google-tag-manager/__tests__/google-tag-manager-node-env.test.js
+++ src/targets/google-tag-manager/__tests__/google-tag-manager-node-env.test.js
@@ -7,6 +7,6 @@ const { GoogleTagManager } = require('../google-tag-manager');
 describe('If window does not exist', () => {
   it('should just silently not send events', () => {
     const events = [{ hitType: 'pageview' }];
-    expect(() => GoogleTagManager(events)).not.toThrow();
+    expect(() => GoogleTagManager()(events)).not.toThrow();
   });
 });
