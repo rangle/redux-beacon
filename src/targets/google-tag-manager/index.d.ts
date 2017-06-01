c9ce07c1a84b712dd4b55061deb0b5002d250caa
Thomas Marek <ttmarek@outlook.com>
2017-06-01 07:36:19 -0400

    v0.4.0 (#104)

--- src/targets/google-tag-manager/index.d.ts
+++ src/targets/google-tag-manager/index.d.ts
@@ -1,7 +1,7 @@
-import { Target } from '../../../';
-
 type Settings = {
   dataLayerName: string,
 };
+
+type Target = (events: any[]) => void;
 
 export function GoogleTagManager(settings?: Settings): Target;
