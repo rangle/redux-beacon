ac0a554dc1153402c910b8a68c95b231f086806c
Chris Parjaszewski <krzysztof.parjaszewski@skygate.pl>
2017-10-26 15:35:20 +0200

    Add React Native support to the logger extension (#148)
    Fixes #147

--- src/extensions/logger/logger.js
+++ src/extensions/logger/logger.js
@@ -22,6 +22,21 @@ const styles = {
 };
 
 /* eslint-disable no-console */
+const groups = [];
+const hr = '-'.repeat(80); // 80 dashes row line
+
+if (!console.group) {
+  console.group = function logGroupStart(label) {
+    groups.push(label);
+    console.log('%c \nBEGIN GROUP: %c', hr, label);
+  };
+}
+if (!console.groupEnd) {
+  console.groupEnd = function logGroupEnd() {
+    console.log('END GROUP: %c\n%c', groups.pop(), hr);
+  };
+}
+
 function logEvents(events) {
   if (events.length > 1) {
     console.group('%c Events:', styles.label);
