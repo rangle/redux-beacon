0ab985b5389dbf354408f5050c0d83e3e1acb72f
Thomas Marek <ttmarek@outlook.com>
2017-11-14 21:49:06 -0500

    Add support for async event definitions

--- src/main/register-events.js
+++ src/main/register-events.js
@@ -1,3 +1,7 @@
+import isPromise from '../utils/is-promise';
+import flatten from 'lodash.flatten';
+
+
 function registerEvents(
   events,
   target,
@@ -13,22 +17,34 @@ function registerEvents(
     }
   };
 
-  if (offlineStorage === undefined) {
-    target(events);
-    ifLoggerLog(events, action, state);
-  } else if (offlineStorage.isConnected(state)) {
-    target(events);
-    ifLoggerLog(events, action, state);
-    offlineStorage.purgeEvents(oldEvents => {
-      if (Array.isArray(oldEvents) && oldEvents.length > 0) {
-        target(oldEvents);
-        ifLoggerLog(oldEvents, null, null, false, true);
-      }
-    });
-  } else {
-    offlineStorage.saveEvents(events);
-    ifLoggerLog(events, action, state, true, false);
-  }
+  const handleEvents = (e) => {
+    if (offlineStorage === undefined) {
+      target(e);
+      ifLoggerLog(e, action, state);
+
+    } else if (offlineStorage.isConnected(state)) {
+      target(e);
+      ifLoggerLog(e, action, state);
+      offlineStorage.purgeEvents(oldEvents => {
+        if (Array.isArray(oldEvents) && oldEvents.length > 0) {
+          target(oldEvents);
+          ifLoggerLog(oldEvents, null, null, false, true);
+        }
+      });
+    } else {
+      offlineStorage.saveEvents(e);
+      ifLoggerLog(e, action, state, true, false);
+    }
+  };
+
+  const asyncEvents = events.filter(isPromise);
+  const syncEvents = events.filter(event => !isPromise(event));
+
+  handleEvents(syncEvents);
+
+  return Promise.all(asyncEvents)
+    .then(flatten)
+    .then(handleEvents);
 }
 
 export default registerEvents;
