436bd541e738261671b22b0ad43fabb24148230a
Thomas Marek <ttmarek@outlook.com>
2017-11-20 21:34:00 -0500

    Replace string.prototype.repeat in logger extension (#159)

--- src/extensions/logger/__tests__/logger.test.js
+++ src/extensions/logger/__tests__/logger.test.js
@@ -3,61 +3,73 @@ import { logger, getTimestamp } from '../';
 
 beforeAll(() => {
   /* eslint-disable no-global-assign */
-  console = makeConsoleMock();
+  console = makeConsoleMock(console);
 });
 
 /* eslint-disable no-console */
-describe('logger()', () => {
-  afterEach(() => {
-    console.clearHistory();
-  });
+afterEach(() => {
+  console.clearHistory();
+});
 
-  Date.now = jest.fn(() => 1489105897006);
-  // toTimeString will return the time in the machine's timezone.
-  // The following stub ensures that the tests will work from any
-  // timezone. This is especially needed to make the tests pass on
-  // circle-ci.
-  /* eslint-disable no-extend-native */
-  Date.prototype.toTimeString = jest.fn(() => '19:31:37 GMT-0500 (EST)');
+Date.now = jest.fn(() => 1489105897006);
+// toTimeString will return the time in the machine's timezone.
+// The following stub ensures that the tests will work from any
+// timezone. This is especially needed to make the tests pass on
+// circle-ci.
+/* eslint-disable no-extend-native */
+Date.prototype.toTimeString = jest.fn(() => '19:31:37 GMT-0500 (EST)');
 
-  const makeAction = () => ({ type: 'ROUTE_CHANGED', payload: '/home' });
+const makeAction = () => ({ type: 'ROUTE_CHANGED', payload: '/home' });
 
-  it('logs actions and any resulting analytics event', () => {
-    const events = [{ hitType: 'pageview', page: '/home' }];
-    const action = makeAction();
-    logger(events, action);
-    expect(console.printHistory()).toMatchSnapshot();
-  });
+it('1. logs actions and any resulting analytics event', () => {
+  const events = [{ hitType: 'pageview', page: '/home' }];
+  const action = makeAction();
+  logger(events, action);
+  expect(console.printHistory()).toMatchSnapshot();
+});
 
-  it('logs actions and any resulting analytics events', () => {
-    const events = [
-      { hitType: 'pageview', page: '/' },
-      { hitType: 'pageview', page: '/home' },
-    ];
-    const action = makeAction();
-    logger(events, action);
-    expect(console.printHistory()).toMatchSnapshot();
-  });
+it('2. logs actions and any resulting analytics events', () => {
+  const events = [
+    { hitType: 'pageview', page: '/' },
+    { hitType: 'pageview', page: '/home' },
+  ];
+  const action = makeAction();
+  logger(events, action);
+  expect(console.printHistory()).toMatchSnapshot();
+});
 
-  it('logs events that are saved offline with a helpful message', () => {
-    const events = [{ hitType: 'pageview', page: '/' }];
-    const action = makeAction();
-    logger(events, action, {}, true);
-    expect(console.printHistory()).toMatchSnapshot();
-  });
+it('3. logs events that are saved offline with a helpful message', () => {
+  const events = [{ hitType: 'pageview', page: '/' }];
+  const action = makeAction();
+  logger(events, action, {}, true);
+  expect(console.printHistory()).toMatchSnapshot();
+});
 
-  it('logs events that were saved offline with a helpful message', () => {
-    const events = [{ hitType: 'pageview', page: '/' }];
-    logger(events, null, null, false, true);
-    expect(console.printHistory()).toMatchSnapshot();
-  });
+it('4. logs events that were saved offline with a helpful message', () => {
+  const events = [{ hitType: 'pageview', page: '/' }];
+  logger(events, null, null, false, true);
+  expect(console.printHistory()).toMatchSnapshot();
+});
 
-  it('logs nothing when there are no events', () => {
-    const events = [];
-    const action = makeAction();
-    logger(events, action);
-    expect(console.printHistory()).toMatchSnapshot();
-  });
+it('5. logs nothing when there are no events', () => {
+  const events = [];
+  const action = makeAction();
+  logger(events, action);
+  expect(console.printHistory()).toMatchSnapshot();
+});
+
+it('6. logs events correctly even if console.group is undefined (RN)', () => {
+  console.group = undefined;
+  console.groupEnd = undefined;
+
+  const events = [
+    { hitType: 'pageview', page: '/' },
+    { hitType: 'pageview', page: '/home' },
+  ];
+
+  const action = makeAction();
+  logger(events, action);
+  expect(console.printHistory()).toMatchSnapshot();
 });
 
 describe('getTimestamp(date)', () => {
