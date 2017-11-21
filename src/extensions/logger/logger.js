98ccd9c67d5e56babebfda369a9df95c6e148fe7
Thomas Marek <ttmarek@outlook.com>
2017-11-20 21:19:32 -0500

    Replace string.prototype.repeat in logger ext.

--- src/extensions/logger/logger.js
+++ src/extensions/logger/logger.js
@@ -1,3 +1,5 @@
+import repeat from 'repeat-string';
+
 function getTimestamp(sinceEpoch) {
   const date = new Date(sinceEpoch);
   return date
@@ -21,41 +23,40 @@ const styles = {
   label: 'color: #93748A; font-weight: bold;',
 };
 
-/* eslint-disable no-console */
-const groups = [];
-const hr = '-'.repeat(80); // 80 dashes row line
-
-if (!console.group) {
-  console.group = function logGroupStart(label) {
-    groups.push(label);
-    console.log('%c \nBEGIN GROUP: %c', hr, label);
-  };
-}
-if (!console.groupEnd) {
-  console.groupEnd = function logGroupEnd() {
-    console.log('END GROUP: %c\n%c', groups.pop(), hr);
-  };
-}
-
-function logEvents(events) {
-  if (events.length > 1) {
-    console.group('%c Events:', styles.label);
-    events.forEach((event, index) => {
-      console.log(`%c (${index + 1})`, styles.label, event);
-    });
-    console.groupEnd('Events');
-  } else if (events.length === 1) {
-    console.log('%c Event:', styles.label, events[0]);
-  }
-}
-
-function logAction(action) {
-  console.log('%c Action:', styles.label, action);
-}
-
 function logger(events, action, state, isSavedOffline, wasSavedOffline) {
   const timestamp = getTimestamp(Date.now());
   const title = `Analytics events @ ${timestamp} ${action ? action.type : ''}`;
+
+  const groups = [];
+  const hr = repeat('-', 80);
+
+  if (!console.group) {
+    console.group = function logGroupStart(label) {
+      groups.push(label);
+      console.log('%c \nBEGIN GROUP: %c', hr, label);
+    };
+  }
+  if (!console.groupEnd) {
+    console.groupEnd = function logGroupEnd() {
+      console.log('END GROUP: %c\n%c', groups.pop(), hr);
+    };
+  }
+
+  function logEvents(events) {
+    if (events.length > 1) {
+      console.group('%c Events:', styles.label);
+      events.forEach((event, index) => {
+        console.log(`%c (${index + 1})`, styles.label, event);
+      });
+      console.groupEnd('Events');
+    } else if (events.length === 1) {
+      console.log('%c Event:', styles.label, events[0]);
+    }
+  }
+
+  function logAction(action) {
+    console.log('%c Action:', styles.label, action);
+  }
 
   if (events.length > 0) {
     if (isSavedOffline) {
