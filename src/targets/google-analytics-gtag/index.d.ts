f6b8c84306fa5d915a3633eeadf0423b82f639ac
Thomas Marek <ttmarek@outlook.com>
2017-11-23 23:24:38 -0500

    Add Typescript interfaces for gtag.js (#166)

--- src/targets/google-analytics-gtag/index.d.ts
+++ src/targets/google-analytics-gtag/index.d.ts
@@ -3,4 +3,18 @@ type Target = (events: any[]) => void;
 /**
  * Creates the Google Analytics Gtag target
  */
-export function GoogleAnalyticsGtag(): Target;
+export function GoogleAnalyticsGtag(trackingId: string): Target;
+
+export interface PageView {
+  type: 'page';
+  trackingId?: string | Array<string>;
+  page_title?: string;
+  page_location?: string;
+  page_path?: string;
+}
+
+export interface Event {
+  type: 'event';
+  name: string;
+  [key: string]: any;
+}
