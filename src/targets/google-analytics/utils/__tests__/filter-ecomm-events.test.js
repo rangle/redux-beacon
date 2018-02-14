1b2db8ba0f032eadc575808222b46e0568caea30
Thomas Marek <ttmarek@outlook.com>
2018-02-14 00:44:06 -0500

    Beautify

--- src/targets/google-analytics/utils/__tests__/filter-ecomm-events.test.js
+++ src/targets/google-analytics/utils/__tests__/filter-ecomm-events.test.js
@@ -2,21 +2,21 @@ import { filterEcommEvents } from '../';
 
 const [name, id, revenue] = ['fooname', 'fooid', 523.55];
 const baseEvent = {
-    hitType: 'addAction',
-    customTrackerId: 'myTracker',
-    ecommType: 'enhanced',
-    actionName: 'click',
-    id,
-    name,
-    revenue,
+  hitType: 'addAction',
+  customTrackerId: 'myTracker',
+  ecommType: 'enhanced',
+  actionName: 'click',
+  id,
+  name,
+  revenue,
 };
 
 it('should return an object with unnecessary keys stripped out', () => {
-    const result = filterEcommEvents(baseEvent);
-    const expected = {
-        id,
-        name,
-        revenue,
-    };
-    expect(result).toEqual(expected);
+  const result = filterEcommEvents(baseEvent);
+  const expected = {
+    id,
+    name,
+    revenue,
+  };
+  expect(result).toEqual(expected);
 });
