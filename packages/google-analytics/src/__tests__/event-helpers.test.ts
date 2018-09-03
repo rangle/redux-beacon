b6d280aaa02ebfd62d78f08c793602f5b675c1a8
Thomas Marek <ttmarek@outlook.com>
2018-09-03 19:06:42 -0400

    Fix linter error

--- packages/google-analytics/src/__tests__/event-helpers.test.ts
+++ packages/google-analytics/src/__tests__/event-helpers.test.ts
@@ -1,8 +1,8 @@
 import {
+  trackEcommAction,
   trackEcommImpression,
   trackEcommProduct,
   trackEcommPromotion,
-  trackEcommAction,
 } from '../event-helpers';
 
 describe('trackEcommImpression', () => {
